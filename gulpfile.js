101b3ebb085417523240743c1425f5cfa1927b22
camilo9090 <camilou9090@gmail.com>
2024-08-02 17:49:52 -0500

    se establecieron los colores para el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
-import { src, dest } from "gulp";
+import { src, dest,watch } from "gulp";
 
 const sass = gulpSass(dartSass);
 
 export function css(done) {
   src("src/scss/app.scss")
     //pipe permite ejecutar la funcion
-    .pipe(sass())
+    //.on('error',sass.logError)muestra errores en consola
+    .pipe(sass().on('error',sass.logError))
     .pipe(dest("build/css"));
   done();
+}
+
+export function dev() {
+watch('src/scss/**/*.scss', css)
+  
 }
